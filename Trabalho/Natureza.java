12eee41fbf057edecbcb9e1c965d021f66de0250
LFCC98 <a81960@alunos.uminho.pt>
2018-05-03 16:44:58 +0100

    abc

--- Trabalho/Natureza.java
+++ Trabalho/Natureza.java
@@ -1,36 +1,6 @@
-public abstract class Natureza{
-    
-    private String tipo;
-    private double deducao;
-    private double limite;
-    
-    public Natureza(){
-        tipo = "";
-        deducao = 0;
-        limite = 0;
-    }
-    
-    public Natureza(String tipo, double ded, double lim){
-        tipo = tipo;
-        deducao = ded;
-        limite = lim;
-    }
-    
-    public Natureza(Natureza n){
-        tipo = n.getTipo();
-        deducao = n.getDeducao();
-        limite = n.getLimite();
-    }
-    
-    public String getTipo(){
-        return tipo;
-    }
-    
-    public double getDeducao(){
-        return deducao;
-    }
-    
-    public double getLimite(){
-        return limite;
-    }
+public interface Natureza
+{
+    public String getTipo();
+    public double getDeducao();
+    public double getLimite();
 }
--- Trabalho/RendasImoveis.java
+++ Trabalho/RendasImoveis.java
@@ -1,18 +1,36 @@
-public class RendasImoveis extends Natureza
+public class RendasImoveis implements Natureza
 {
+    private String tipo;
+    private double limite;
+    private double deducao;
+    
     public RendasImoveis(){
-       super("Saude", 0.25, 5000);
+       tipo = "";
+       limite = 0;
+       deducao = 0;
+    }
+    
+    public RendasImoveis(String type, double lim, double deduz){
+        tipo = type;
+        limite = lim;
+        deducao = deduz;
+    }
+    
+    public RendasImoveis(RendasImoveis r){
+        tipo = r.getTipo();
+        limite = r.getLimite();
+        deducao = r.getDeducao();
     }
     
     public String getTipo(){
-        return super.getTipo();
+        return tipo;
     }
     
     public double getDeducao(){
-        return super.getDeducao();
+        return deducao;
     }
     
     public double getLimite(){
-        return super.getLimite();
+        return limite;
     }
 }
