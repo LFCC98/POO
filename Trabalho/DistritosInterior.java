8bbdf4bbb10711568c4d627915e311850df06d01
PedroCapa <a83170@alunos.uminho.pt>
2018-05-26 16:30:25 +0100

    Empresas do interior

---
+++ Trabalho/DistritosInterior.java
@@ -0,0 +1,114 @@
+import java.util.*;
+import java.io.*;
+
+public class DistritosInterior implements Serializable
+{
+    private List<String> distritos;
+
+    /** Constroi uma lista vazia*/
+    public DistritosInterior(){
+        List<String> distritos = new ArrayList<>();
+    }
+    
+    /** Construtor parametrizado*/
+    public DistritosInterior(List<String> dist){
+        this.distritos = dist;
+    }
+    
+    /** Constroi uma nova lista*/
+    public DistritosInterior(DistritosInterior di){
+        distritos = di.getDist();
+    }
+    
+    /**
+     * Metodo que devolve a lista de distritos do interior
+     * 
+     * @return A lista de distritos
+     */
+    public List<String> getDist(){
+        return distritos;
+    }
+    
+    /**
+     * Metodo que altera a lista de distritos do interior
+     * 
+     * @param empInt A nova lista de distritos
+     */
+    public void setDist(List<String> distInt){
+        distritos = distInt;
+    }
+    
+    /** 
+     * Metodo que faz copia de um DistritoInterior com diferentes posiçoes de memoria
+     *
+     *@return Uma copia de um DistritoInterior
+     */
+    public DistritosInterior clone(){
+        return new DistritosInterior(this);
+    }
+    
+    /** 
+     * Metodo que transforma um DistritoInterior numa String
+     * 
+     * @return uma String de um DistritoInterior
+     */
+    public String toString (){
+        String s = " Distritos do interior: " + getDist();
+        return s;
+    }
+    
+    /** 
+     * Metodo que verifica se um DistritoInterior e igual a um objeto
+     * 
+     * @return uma String de um DistritoInterior
+     */
+    public boolean equals(Object o){
+        if(o == this)
+            return true;
+        if(o == null || o.getClass() != this.getClass())
+            return false;
+        DistritosInterior i = (DistritosInterior)o;
+        if(super.equals(i) && distritos == i.getDist())
+            return true;
+        return false;
+    }
+    
+    /** Metodo que adiciona um distrito a uma lista de distritos do interior
+     * 
+     * @param s Distrito a adicionar à lista de distritos do interior
+     */
+    public void adicionaDistrito(String s) throws JaExisteDistritoException{
+        if(distritos.contains(s)){
+            throw new JaExisteDistritoException("O distrito já pertence à lista");
+        }
+        distritos.add(s);
+    }
+    
+    /**
+     * Metodo que guarda num ficheiro um Sistema que contem todas as faturas, Contribuintes e Empresas
+     * 
+     * @param nomeFichiro Ficheio que serao guardados os dados do sistema
+     */
+    public void guardaEstado(String nomeFicheiro) throws FileNotFoundException, IOException{
+        FileOutputStream fos = new FileOutputStream(nomeFicheiro);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(this);
+        oos.flush();
+        oos.close();
+    }
+    /**
+     * Metodo que carrega de um ficheiro um Sistema com todas as faturas, Contribuintes e Empresas 
+     * 
+     * @param nomeFicheiro Ficheiro em que estao guardadas as informações sobre o sistema
+     * 
+     * @return um Sistema
+     */
+    public DistritosInterior carregaEstado(String nomeFicheiro) throws FileNotFoundException, IOException, ClassNotFoundException{
+        FileInputStream fis = new FileInputStream(nomeFicheiro);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        DistritosInterior di = (DistritosInterior) ois.readObject();
+        ois.close();
+        return di;
+    }
+    
+}
---
+++ Trabalho/EmpresasInterior.java
@@ -0,0 +1,67 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.*;
+import java.io.*;
+
+public class EmpresasInterior extends Empresas implements Desconto, Serializable
+{
+    /** Constroi uma EmpresaInterior vazia*/
+    public EmpresasInterior(){
+        super();
+    }
+    
+    /** Construtor parametrizado*/
+    public EmpresasInterior(int nif, String e, String nome, String morada, String pass, Set<Natureza> nat, double deducao){
+        super(nif, e, nome, morada, pass, nat, deducao);
+    }
+    
+    /** Constroi uma nova EmpresaInterior*/
+    public EmpresasInterior(EmpresasInterior ei){
+        super(ei);
+    }
+    
+    /** 
+     * Metodo que faz copia de uma EmpresaInterior com diferentes posiçoes de memoria
+     *
+     *@return Uma copia de uma EmpresaInterior
+     */
+    public EmpresasInterior clone(){
+        return new EmpresasInterior(this);
+    }
+    
+    /** 
+     * Metodo que transforma uma EmpresaInterior numa String
+     * 
+     * @return uma String de uma EmpresaInterior
+     */
+    public String toString(){
+        String s = super.toString();
+        return s;
+    }
+    
+    /** 
+     * Metodo que verifica se um EmpresaInterior e igual a um objeto
+     * 
+     * @return uma String de um EmpresaInterior
+     */
+    public boolean equals(Object o){
+        if(o == this)
+            return true;
+        if(o == null || o.getClass() != this.getClass())
+            return false;
+        EmpresasInterior i = (EmpresasInterior)o;
+        if(super.equals(i))
+            return true;
+        return false;
+    }
+    
+    /** 
+     * Metodo que calcula a percentagem que ira aumentar pela quantidade de filhos
+     * 
+     * @return Um double com a percentagem que ira aumentar 
+     */
+    public double reducaoImposto(){
+            return 1.1;
+    }
+}
